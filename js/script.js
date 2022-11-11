06508c8f5107c718b3f8fcebe8242432edd22e40
Eli Olmos <eliolmos81@gmail.com>
2022-11-11 16:17:54 -0300

    modifico según las sugerencias de mi tutor corrector

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ class Reserva {
 const reservas = [
   new Reserva(25, 11, 2022, 13, 2, 1),
   new Reserva(26, 11, 2022, 18, 3, 2),
+  new Reserva(25, 12, 2022, 15, 2, 3),
 ];
 
 function login() {
@@ -44,7 +45,7 @@ function login() {
 
 if (login()) {
   let opcion = prompt(
-    "Elige una opción para comenzar: \n1- Reservar tu cancha. \n2 - Consultar todas tu reservas  \nPresioná S para finalizar."
+    "Elige una opción para comenzar: \n1- Reservar tu cancha. \n2 - Consultar todas tu reservas \n3 - Consultar las reservas por dia, mes o año.  \nPresioná S para finalizar."
   );
 
   while (opcion != "S" && opcion != "s") {
@@ -104,11 +105,78 @@ if (login()) {
           alert(
             "A continuación le mostramos todas las reservas para el usuario con DNI: " +
               savedDNI +
-              "\n\n"  + (MostrarReservas(reservas)));
-
+              "\n\n" +
+              MostrarReservas(reservas)
+          );
         }
         break;
+      case "3":
+        alert("seleccione un opción para poder buscar tus reservas");
+        let filtrado = prompt(
+          "Elige una opción para comenzar: \n1- Día \n2 - Mes  \nPresioná M para finalizar."
+        );
+        while (filtrado != "M" && filtrado != "m") {
+          switch (filtrado) {
+            case "1":
+              let diaIngresado = prompt("Ingresa el día a buscar");
+              diaIngresado =
+                diaIngresado[0].toUpperCase() + diaIngresado.substring(1);
 
+              const filtroDia = reservas.filter(
+                (reservas) => reservas.dia == diaIngresado
+              );
+              for (const dia of filtroDia) {
+                alert(
+                  "Sus reservas correspondientes al dia ingresado (" +
+                    diaIngresado +
+                    ") " +
+                    "es o son: " +
+                    dia.dia +
+                    "/" +
+                    dia.mes +
+                    "/" +
+                    dia.anio +
+                    " a las " +
+                    dia.hora +
+                    " hs."
+                );
+                console.log(filtroDia);
+              }
+              break;
+            case "2":
+              let mesIngresado = prompt("Ingresa el día a buscar");
+              mesIngresado =
+                mesIngresado[0].toUpperCase() + mesIngresado.substring(1);
+
+              const filtroMes = reservas.filter(
+                (reservas) => reservas.mes == mesIngresado
+              );
+              for (const mes of filtroMes) {
+                alert(
+                  "Sus reservas correspondientes al me ingresado ( " +
+                    mesIngresado +
+                    ") " +
+                    "es o son: " +
+                    mes.dia +
+                    "/" +
+                    mes.mes +
+                    "/" +
+                    mes.anio +
+                    " a las " +
+                    mes.hora +
+                    " hs."
+                );
+                console.log(filtromes);
+              }
+
+              break;
+            default:
+              alert("Elegiste una opción inválida");
+              break;
+          }
+          break;
+        }
+        break;
       default:
         alert("Elegiste una opción inválida");
         break;
@@ -124,19 +192,20 @@ if (login()) {
 }
 
 function MostrarReservas(array) {
-  let infoReservas = '';
+  let infoReservas = "";
   array.forEach((elemento) => {
     infoReservas +=
       elemento.dia +
-      '/' +
+      "/" +
       elemento.mes +
-      '/' +
+      "/" +
       elemento.anio +
-      ' a las ' +
+      " a las " +
       elemento.hora +
-      ' hs y con una duración del alquiler de ' +
-      elemento.duracion + ' hora/s'+
-      '\n\n';
+      " hs y con una duración del alquiler de " +
+      elemento.duracion +
+      " hora/s" +
+      "\n\n";
   });
 
   return infoReservas;
